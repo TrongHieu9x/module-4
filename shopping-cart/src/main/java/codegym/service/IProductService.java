37b345edab9950931de3907464de582e48928979
TrongHieu9x <tronghieu4101999@gmail.com>
2022-03-04 15:08:01 +0700

    thuc hanh

---
+++ shopping-cart/src/main/java/codegym/service/IProductService.java
@@ -0,0 +1,13 @@
+package codegym.service;
+
+import codegym.model.Product;
+
+import java.util.Optional;
+
+public interface IProductService {
+
+    Iterable<Product> findAll();
+
+    Optional<Product> findById(Long id);
+
+}
---
+++ shopping-cart/src/main/java/codegym/service/ProductService.java
@@ -0,0 +1,25 @@
+package codegym.service;
+
+import codegym.model.Product;
+import codegym.repository.IProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ProductService implements IProductService {
+
+    @Autowired
+    private IProductRepository iProductRepository;
+
+    @Override
+    public Iterable<Product> findAll() {
+        return iProductRepository.findAll();
+    }
+
+    @Override
+    public Optional<Product> findById(Long id) {
+        return iProductRepository.findById(id);
+    }
+}
